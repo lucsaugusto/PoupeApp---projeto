69766ebdb3cdb4d3e71fe61c0c437845255d130d
lucsaugusto <lucasaugustoneves@hotmail.com>
2019-12-20 09:01:48 -0300

    Adicionando rotas

--- poupe-app/src/app/app-routing.module.ts
+++ poupe-app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './component/contact/contact.component'
+import { AboutUsComponent } from './component/about-us/about-us.component';
 
 const routes: Routes = [
+  {path: 'contact', component: ContactComponent},
+  {path: 'about-us', component: AboutUsComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
