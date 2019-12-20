52d8938b7dc0e1e95807245a1dc15086772b0ede
lucsaugusto <lucasaugustoneves@hotmail.com>
2019-12-20 15:41:58 -0300

    Entrega Task 5

--- poupe-app/src/app/app-routing.module.ts
+++ poupe-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutUsComponent } from './component/about-us/about-us.component';
 import { PostsComponent } from './component/posts/posts.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
   {path: 'contact', component: ContactComponent},
   {path: 'about-us', component: AboutUsComponent},
   {path: 'posts', component: PostsComponent}
